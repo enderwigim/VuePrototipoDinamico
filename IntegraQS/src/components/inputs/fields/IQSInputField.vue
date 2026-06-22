a22d691baf36920ad07982a2dc317bf6290da0cd
Santiago Lorenzano <enderwigim@gmail.com>
2026-06-22 13:30:13 +0200

    Separación entre Inputs Base y InputField

---
+++ IntegraQS/src/components/inputs/fields/IQSInputField.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="flex flex-col gap-1.5">
+    <label v-if="label" :for="inputIdFinal" class="text-sm font-medium text-slate-700">
+      {{ label }}
+
+      <span v-if="required" class="text-red-500"> * </span>
+    </label>
+
+    <slot :id="inputIdFinal" :describedBy="describedBy" />
+
+    <small v-if="helpText" :id="helpId" class="text-xs text-slate-500">
+      {{ helpText }}
+    </small>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+interface Props {
+  label?: string;
+  inputId?: string;
+  required?: boolean;
+  helpText?: string;
+}
+
+const props = defineProps<Props>();
+// En caso de que el usuario no haya proporcionado un ID para el input, generamos uno aleatorio para asegurarnos de que el label esté correctamente asociado al input.
+const generatedId = "iqs-input-" + Math.random().toString(36).substring(2, 10);
+// Computamos el ID final.
+const inputIdFinal = computed(function () {
+  if (props.inputId) {
+    return props.inputId;
+  }
+
+  return generatedId;
+});
+
+// Computamos el ID del elemento de ayuda, que se usará en el atributo aria-describedby del input para mejorar la accesibilidad.
+const helpId = computed(function () {
+  return inputIdFinal.value + "-help";
+});
+// Computamos el valor del atributo aria-describedby, que solo se incluirá si hay texto de ayuda disponible.
+const describedBy = computed(function () {
+  if (props.helpText) {
+    return helpId.value;
+  }
+
+  return undefined;
+});
+</script>
