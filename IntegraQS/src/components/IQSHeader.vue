69ba9315f73ec8bf29caa61b8573a32b8a7c70f9
Santiago Lorenzano <enderwigim@gmail.com>
2026-06-22 17:35:07 +0200

    Checkbox added

--- IntegraQS/src/components/IQSHeader.vue
+++ IntegraQS/src/components/IQSHeader.vue
@@ -34,12 +34,20 @@
       </IQSInputMaskBase>
 
       <IQSSelectBase
-        v-if="field.type === 'select'"
-        :model-value="getModelValue(field)"
-        @update:model-value="setModelValue(field, $event)"
+        v-if="field.type === 'select' || field.type === 'select linked'"
+        :model-value="getSelectModelValue(field)"
+        @update:model-value="setSelectModelValue(field, $event)"
         :placeholder="field.placeholder"
         :options="field.options"
+        option-label="title"
+        option-value="value"
       ></IQSSelectBase>
+
+      <IQSCheckBoxBase
+        v-if="field.type === 'checkbox'"
+        :model-value="getModelValue(field)"
+        @update:model-value="setModelValue(field, $event)"
+      ></IQSCheckBoxBase>
     </IQSInputField>
   </div>
 </template>
@@ -52,6 +60,7 @@ import IQSInputMaskBase from "@/components/inputs/base/IQSInputMaskBase.vue";
 import IQSInputNumberBase from "@/components/inputs/base/IQSInputNumberBase.vue";
 import IQSInputTextBase from "@/components/inputs/base/IQSInputTextBase.vue";
 import IQSSelectBase from "@/components/inputs/base/IQSSelectBase.vue";
+import IQSCheckBoxBase from "@/components/inputs/base/IQSCheckBoxBase.vue";
 
 export type FieldValue = string | number | boolean | null;
 
@@ -100,7 +109,7 @@ const props = withDefaults(
     model: () => ({}),
   },
 );
-
+console.log(props.model);
 const emit = defineEmits<{
   (e: "update:model", value: DynamicModel): void;
 }>();
@@ -122,6 +131,37 @@ function setModelValue(field: HeaderField, value: FieldValue): void {
     ...props.model,
     [field.field]: value,
   });
+}
+
+// CASO SELECT
+function getSelectModelValue(field: HeaderField): FieldValue {
+  const value = getModelValue(field);
+
+  if (!field.options) {
+    return value;
+  }
+
+  const option = field.options.find((option) => {
+    return String(option.value) === String(value);
+  });
+
+  if (!option) {
+    return value;
+  }
+
+  return option.value;
+}
+
+function setSelectModelValue(field: HeaderField, value: FieldValue): void {
+  const currentValue = getModelValue(field);
+
+  let newValue = value;
+
+  if (typeof currentValue === "number" && value !== null) {
+    newValue = Number(value);
+  }
+
+  setModelValue(field, newValue);
 }
 // const modelComp = computed({
 //   get: () => props.model,
---
+++ IntegraQS/src/components/inputs/base/IQSCheckBoxBase.vue
@@ -0,0 +1,82 @@
+<template>
+  <VoltCheckbox v-bind="checkboxAttrs" />
+</template>
+
+<script setup lang="ts">
+import { computed, useAttrs } from "vue";
+import VoltCheckbox from "@/volt/Checkbox.vue";
+
+defineOptions({
+  inheritAttrs: false,
+});
+
+const attrs = useAttrs();
+
+const checkboxPt = {
+  "pt:root:class": `
+    relative
+    inline-flex
+    select-none
+    align-bottom
+  `,
+
+  "pt:input:class": `
+    cursor-pointer
+    disabled:cursor-not-allowed
+  `,
+
+  "pt:box:class": `
+    flex
+    items-center
+    justify-center
+
+    w-5
+    h-5
+
+    rounded-md
+    border
+    border-slate-300
+
+    bg-white
+    text-white
+
+    shadow-sm
+    transition-colors
+    duration-200
+
+    peer-enabled:peer-hover:border-blue-400
+
+    p-checked:border-blue-500
+    p-checked:bg-blue-500
+    p-checked:text-white
+
+    peer-enabled:peer-hover:p-checked:bg-blue-600
+    peer-enabled:peer-hover:p-checked:border-blue-600
+
+    peer-focus-visible:outline
+    peer-focus-visible:outline-2
+    peer-focus-visible:outline-offset-2
+    peer-focus-visible:outline-blue-500
+
+    p-invalid:border-red-500
+
+    p-disabled:bg-slate-100
+    p-disabled:border-slate-300
+    p-disabled:text-slate-400
+  `,
+
+  "pt:icon:class": `
+    w-3.5
+    h-3.5
+    text-white
+  `,
+};
+
+const checkboxAttrs = computed(function () {
+  return {
+    binary: true,
+    ...attrs,
+    ...checkboxPt,
+  };
+});
+</script>
