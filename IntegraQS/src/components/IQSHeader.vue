48c0d7020750c28971d9fd1788598da5125cc091
Santiago Lorenzano <enderwigim@gmail.com>
2026-07-14 14:50:46 +0200

    Reapply "Changes into SelectBase"
    This reverts commit b39b61362383a8645f277fa7623a477628b13cf1.

--- IntegraQS/src/components/IQSHeader.vue
+++ IntegraQS/src/components/IQSHeader.vue
@@ -42,6 +42,7 @@
         :placeholder="field.placeholder"
         :readonly="field.state === 'readOnly'"
         :options="field.options"
+        :parent-field="field.field"
         option-label="title"
         option-value="value"
       ></IQSSelectBase>
